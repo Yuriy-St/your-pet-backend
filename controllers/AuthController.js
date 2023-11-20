4828a38787c8b5032914297fcda055e167a5d107
Yuriy-St <yuriystaynov@gmail.com>
2023-11-20 09:49:44 +0200

    refactor: authorization
    Improved in order to get token after registration

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,22 +1,23 @@
 const asyncHandler = require('../helpers/asyncHandler');
 const AuthService = require('../services/AuthService');
-const gravatar = require('gravatar');
-const fs = require('fs/promises');
-const path = require('path');
+const fileController = require('./FileController');
 
 class AuthController {
-  avatarsDir = path.join(__dirname, '..', 'public', 'avatars');
-
   register = asyncHandler(async (req, res) => {
-    const { name, email, avatarURL } = await AuthService.register({
-      ...req.body,
-      avatarURL: gravatar.url(req.body.email),
-    });
+    const { name, email, birthday, phone, city, avatarURL } =
+      await AuthService.register({
+        ...req.body,
+        avatarURL: process.env.AVATAR_DEFAULT_URL,
+      });
+    const { token } = await AuthService.login(req.body);
 
     res.status(201).json({
       code: 201,
       message: 'User registered successfully.',
-      data: { name, email, avatarURL },
+      data: {
+        user: { name, email, birthday, phone, city, avatarURL },
+        token,
+      },
     });
   });
 
@@ -48,28 +49,36 @@ class AuthController {
   });
 
   update = asyncHandler(async (req, res) => {
-    const { name, email, birthday, phone, city, avatarURL } =
-      await AuthService.update(req.user._id, req.body);
-    res.status(200).json({
-      code: 200,
-      message: 'User updated successfully',
-      data: { name, email, birthday, phone, city, avatarURL },
+    // const { name, email, birthday, phone, city, avatarURL } =
+    //   await AuthService.update(req.user._id, req.body);
+    // res.status(200).json({
+    //   code: 200,
+    //   message: 'User updated successfully',
+    //   data: { name, email, birthday, phone, city, avatarURL },
+    // });
+    res.status(500).json({
+      code: 500,
+      message: 'User update under consturction',
     });
   });
 
   updateAvatar = asyncHandler(async (req, res) => {
-    const { _id } = req.user;
-    const { path: tempDir, originalname } = req.file;
-    const filename = `${_id}_${originalname}`;
-    const avatarPath = path.join(this.avatarsDir, filename);
-    await fs.rename(tempDir, avatarPath);
-    const avatarURL = path.join('avatars', filename);
-    await AuthService.update(_id, { avatarURL });
+    // const { _id } = req.user;
+    // const { path: tmpPath, originalname } = req.file;
+    // const filename = `${_id}_${originalname}`;
 
-    res.status(200).json({
-      code: 200,
-      message: 'User avatar updated successfully',
-      data: { avatarURL },
+    // const avatarURL = fileController();
+    // await AuthService.update(_id, { avatarURL });
+
+    // res.status(200).json({
+    //   code: 200,
+    //   message: 'User avatar updated successfully',
+    //   data: { avatarURL },
+    // });
+
+    res.status(500).json({
+      code: 500,
+      message: 'User avatar update under consturction',
     });
   });
 }

0b27d488492879ad344c80c295a5c00660846f24
Yuriy-St <yuriystaynov@gmail.com>
2023-11-26 01:46:46 +0200

    feature: add notice

--- schemas/noticeSchemas.js
+++ schemas/noticeSchemas.js
@@ -1,10 +1,34 @@
 const Joi = require('joi');
 
-// add your code here
+const addNoticeSchema = Joi.object({
+  category: Joi.string().required(),
+  title: Joi.string().required(),
+  location: Joi.string().required(),
+  price: Joi.alternatives().conditional('category', {
+    is: 'sell',
+    then: Joi.number().required(),
+  }),
+  name: Joi.string().required(),
+  type: Joi.string().required(),
+  birthDate: Joi.alternatives().conditional('category', {
+    not: 'found',
+    then: Joi.number().required(),
+    otherwise: Joi.number().optional(),
+  }),
+  sex: Joi.string(),
+  comments: Joi.string(),
+});
+
+const updateNoticeSchema = Joi.object({
+  category: Joi.string(),
+  name: Joi.string(),
+  type: Joi.string(),
+  birthDate: Joi.date(),
+  sex: Joi.string(),
+  comments: Joi.string(),
+});
 
 module.exports = {
-  registerSchema,
-  loginSchema,
-  updateSchema,
-  verifyEmailSchema,
+  addNoticeSchema,
+  updateNoticeSchema,
 };

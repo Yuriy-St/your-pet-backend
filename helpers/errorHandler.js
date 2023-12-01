4ca913c6931ec573b7691164a6259dd955f54dc8
Yuriy-St <yuriystaynov@gmail.com>
2023-12-01 11:19:26 +0200

    feature: togle favorite notice

--- helpers/errorHandler.js
+++ helpers/errorHandler.js
@@ -2,5 +2,7 @@ module.exports = (err, req, res, next) => {
   const statusCode = err.status || res.code || 500;
   const { message = 'Server error' } = err;
   const errorName = err.name || 'unknown error';
+  const { stack } = err;
+  //TODO: add sack to log
   res.status(statusCode).json({ code: statusCode, errorName, message });
 };
